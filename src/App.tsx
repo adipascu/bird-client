b699e0cbdef11a7a490af026bad3eb52bf62aefb
Adrian Pascu <mail@adipascu.ro>
2023-05-13 18:35:05 +0200

    Show component example

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,27 @@
-import type { Component } from "solid-js";
-
-type HumanBeing = {
-  type: "human";
-  firstName: string;
-  lastName: string;
-  birthDate: Date;
-};
-
-type AlienBeing = {
-  type: "alien";
-  planet: string;
-  birthNumber: number;
-};
-
-type Being = HumanBeing | AlienBeing;
-
-const myself: HumanBeing = {
-  type: "human",
-  firstName: "Adrian",
-  lastName: "Pascu",
-  birthDate: new Date(1992, 1, 1),
-};
-
-const logBeingData = (being: Being) => {
-  if (being.type === "human") {
-    console.log("The being is a human");
-    console.log("First name " + being.firstName);
-    console.log("Last name " + being.lastName);
-    console.log("Birth date " + being.birthDate);
-  } else {
-    console.log("Planet " + being.planet);
-    console.log("Birth number " + being.birthNumber);
-  }
-};
-
-logBeingData({
-  type: "human",
-  firstName: "Adrian",
-  lastName: "Pascu",
-  birthDate: new Date(1992, 1, 1),
-});
+import { Component, Show, createEffect, createSignal } from "solid-js";
 
 const App: Component = () => {
-  return <div>Hello world</div>;
+  const [getCount, setCount] = createSignal(0);
+  console.log("Render " + Math.random());
+  console.log("Render: CurrentCount is", getCount());
+  //expesinve calculation digitOfPi(1000)
+
+  createEffect(() => {
+    getCount();
+    if (Math.random() > 0.1) {
+      console.log("Effect: CurrentCount is", getCount());
+    }
+  });
+
+  return (
+    <Show when={getCount() < 10} fallback={<h1>The count is too big</h1>}>
+      <div>
+        The count is:{getCount()}
+        <br />
+        <button onClick={() => setCount(getCount() + 1)}>Increment</button>
+      </div>
+    </Show>
+  );
 };
 
 export default App;
